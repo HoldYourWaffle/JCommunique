2ff52bb0e985107c2d915a3c6691a33289863221
HoldYourWaffle <ravivanrooijen@live.nl>
2018-02-17 22:32:41 +0000

    Bump version

--- rundemo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Assembles and runs the demo specified by the command line argument.
-#The command line argument should be the name of the file with the main in it.
-
-gradle assemble
-cd build/classes/main
-java com.demo.$1
